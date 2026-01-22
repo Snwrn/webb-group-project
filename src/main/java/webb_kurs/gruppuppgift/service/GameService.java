d17f7642c70149728369f0a5ace11db8d23de214
SaliSammoArtin <Salisammo@live.se>
2026-01-22 11:22:01 +0100

    small changes

--- src/main/java/webb_kurs/gruppuppgift/service/GameService.java
+++ src/main/java/webb_kurs/gruppuppgift/service/GameService.java
@@ -1,4 +1,20 @@
 package webb_kurs.gruppuppgift.service;
 
-public class GameService {
+import org.springframework.stereotype.Service;
+import webb_kurs.gruppuppgift.repository.GameRepository;
+
+@Service
+public class GameService extends GameRepository {
+
+    /*
+    public Game createGame(){}
+
+     */
+     /*
+    getAllGames()
+    getGameById(UUID/long?)
+    createGame()
+    updateGame(id)
+    deleteGame(id)
+      */
 }
