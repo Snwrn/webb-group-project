790fb30a3b43c06594e8c9a2d4d90c3065428be8
SaliSammoArtin <Salisammo@live.se>
2026-01-23 12:55:00 +0100

    methods in gameservice

--- src/main/java/webb_kurs/gruppuppgift/service/GameService.java
+++ src/main/java/webb_kurs/gruppuppgift/service/GameService.java
@@ -6,15 +6,4 @@ import webb_kurs.gruppuppgift.repository.GameRepository;
 @Service
 public class GameService extends GameRepository {
 
-    /*
-    public Game createGame(){}
-
-     */
-     /*
-    getAllGames()
-    getGameById(UUID/long?)
-    createGame()
-    updateGame(id)
-    deleteGame(id)
-      */
 }
